0a57bd50eccf0c30b799496736cbc239c119244c
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-02-20 16:25:58 +0100

    Add buggy cabal files retriever.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
 module Main where
 
+import Control.Monad.State
+import qualified Data.Text as T
+
 import Stackage.StackageTypes
 
+class Loggable a where
+    log :: a -> Text
+
+class ComputationStep where
+    compute :: IO ()
+
+data Computations = Computations {
+    steps :: [ComputationStep],
+    progress :: Int,
+    current :: ComputationStep
+}
+
+instance Loggable Computations where
+    log (Computations cSteps p c) = T.concat ["[Step ", p, "/", length cSteps, "]", log c]
+
+data CabalFiles = CabalFiles {
+    urls :: [Text]
+}
+
+
 main :: IO ()
-main = undefined
+main = do
+  stackageYaml <- undefined
+  let packages = getHackageCabalUrl <$> parseStackageYaml stackageYaml
+  putStrLn packages
+  
--- src/Stackage/StackageParser.hs
+++ src/Stackage/StackageParser.hs
@@ -4,9 +4,22 @@ module Stackage.StackageParser (
 
 import Data.Yaml (decode)
 import Data.Text  (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding as E
 
 import Stackage.StackageTypes (Packages(..))
 
 parseStackageYaml :: Text -> Maybe Packages
 parseStackageYaml = decode . E.encodeUtf8
+
+getHackageCabalUrl :: Packages -> [Text]
+getHackageCabalUrl = foldlWithKey getCabal ""
+  where getCabal xs k e = T.concat ["https://hackage.haskell.org/package/", 
+                                    packName
+                                    "-",
+                                    ppVersion e,
+                                    "/",
+                                    packName,
+                                    ".cabal"]
+          where
+            !packName = T.pack $ unPackageName k
