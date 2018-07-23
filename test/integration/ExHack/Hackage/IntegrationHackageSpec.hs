20344f1cf12c39da2a4f9e9f0609b900f3ea163a
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-07-23 18:47:36 +0200

    Test E2E exports extraction.

--- test/integration/ExHack/Hackage/IntegrationHackageSpec.hs
+++ test/integration/ExHack/Hackage/IntegrationHackageSpec.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module ExHack.Hackage.IntegrationHackageSpec (spec) where
 
-import Data.Maybe (isNothing)
+import Data.Maybe (isNothing, fromJust)
 import Data.FileEmbed (embedFile)
 import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 
-import ExHack.Config (config)
-import ExHack.Hackage.Hackage (unpackHackageTarball)
+import ExHack.Cabal.CabalParser(parseCabalFile, getSuccParse)
+import ExHack.Config (StackConfig(..))
+import ExHack.Ghc (getModExports)
+import ExHack.Hackage.Hackage (unpackHackageTarball, getTarballDesc,
+                              loadExposedModules)
 import ExHack.Stackage.Stack(build)
 
-
+config :: StackConfig
+config = StackConfig {_nbJobs=4, _workDir=".stack-work/", _stackRoot="/home/ninjatrappeur/exhst", _stackBin=Just "/nix/store/4s5l0p1s906prwsapm2g083cp5z4haxl-stack-1.6.5/bin/stack", _tarballsDir="/home/ninjatrappeur/exhst/tb/"}
 
 spec :: Spec
 spec = describe "hackage" $ do
@@ -20,5 +25,15 @@ spec = describe "hackage" $ do
           tbp <- unpackHackageTarball "./test/integration/workdir/" $(embedFile "./test/integration/fixtures/timeit.tar.gz")
           r <- build config tbp
           r `shouldSatisfy` isNothing
-
-          
+        it "should retrieve a package exports" $ do
+          tbp <- unpackHackageTarball "./test/integration/workdir/" $(embedFile "./test/integration/fixtures/timeit.tar.gz")
+          _ <- build config tbp
+          tbdm <- fromJust <$> getTarballDesc tbp
+          let p = head $ getSuccParse [parseCabalFile tbdm]
+          mods <- loadExposedModules p
+          let exports = getModExports <$> mods
+          exports `shouldBe` []
+          -- 1: Parse cabal file.
+          -- 2: Load file in GHC
+          -- 3: Get a desugared modules.
+          -- 4: Get package exports. -}
--- test/integration/ExHack/Stack/IntegrationStackSpec.hs
+++ test/integration/ExHack/Stack/IntegrationStackSpec.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ExHack.Stack.IntegrationStackSpec (spec) where
 
 import Data.Maybe (isNothing)
 import Test.Hspec (Spec, describe, it, shouldSatisfy)
 
 import qualified ExHack.Stackage.Stack as Stack (setup)
-import ExHack.Config (config)
+import ExHack.Config (StackConfig(..))
+
+config :: StackConfig
+config = StackConfig {_nbJobs=4, _workDir=".stack-work/", _stackRoot="/home/ninjatrappeur/exhst", _stackBin=Just "/nix/store/4s5l0p1s906prwsapm2g083cp5z4haxl-stack-1.6.5/bin/stack", _tarballsDir="/home/ninjatrappeur/exhst/tb/"}
 
 spec :: Spec
 spec = describe "stack" $ 
