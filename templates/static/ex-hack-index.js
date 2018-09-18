7e02c3f1040fe42dfe7c22449a1a771a07adc0d2
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-09-18 15:47:42 +0200

    Implement package list template.

---
+++ templates/static/ex-hack-index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Search for a package name on the front-page.
+  var list = new List('package-table', {
+    valueNames: ['packageName']
+  });
+});
