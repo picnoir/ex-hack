bdfd35007add4a3961619dd271cd82b14e05b527
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-09-19 10:41:17 +0200

    Written package HTML template.

--- templates/static/ex-hack-index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Search for a package name on the front-page.
-  var list = new List('package-table', {
-    valueNames: ['packageName']
-  });
-});
